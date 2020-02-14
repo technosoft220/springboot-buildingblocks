1af35d4ac6283dc19b68d1df1bd0e9723957eb24
moanand <moh.civil180@gmail.com>
2020-02-14 11:01:50 +0530

    First commit

---
+++ src/test/java/com/technosoft/SpringbootBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technosoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
