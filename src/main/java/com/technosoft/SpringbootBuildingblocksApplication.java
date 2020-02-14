f2602712becb06d79cae3f083fd32de9df05b673
moanand <moh.civil180@gmail.com>
2020-02-14 11:39:05 +0530

    Second commit

--- src/main/java/com/technosoft/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/technosoft/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.technosoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
