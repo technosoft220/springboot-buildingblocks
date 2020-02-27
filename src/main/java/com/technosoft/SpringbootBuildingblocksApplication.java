fe934116d8f07a7b228ac89057dd93d1a1bb737f
moanand <moh.civil180@gmail.com>
2020-02-27 10:58:44 +0530

    I18N first commit

--- src/main/java/com/technosoft/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/technosoft/SpringbootBuildingblocksApplication.java
@@ -1,13 +1,33 @@
 package com.technosoft;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.*;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver headerLocaleResolver = new AcceptHeaderLocaleResolver();
+		headerLocaleResolver.setDefaultLocale(Locale.US);
+		return headerLocaleResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		return source;
 	}
 
 }
--- src/main/java/com/technosoft/hello/HelloWorldController.java
+++ src/main/java/com/technosoft/hello/HelloWorldController.java
@@ -1,6 +1,12 @@
 package com.technosoft.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 /*import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;*/
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +14,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+
 	// @RequestMapping(path = "/helloworld",method = RequestMethod.GET)
-	@GetMapping("helloWorld")
+	@GetMapping("/helloWorld")
 	public String helloWorld() {
 		return "Hello World!";
 	}
 
-	@GetMapping("helloWorld-bean")
+	@GetMapping("/helloWorld-bean")
 	public UserDetails helloWorldbean() {
 		return new UserDetails("Ansh", "Anand", "Bangalore");
+	}
+
+	@GetMapping("/hello-int")
+	public String getI18NMessages(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+
+	@GetMapping("/hello-int1")
+	public String getI18NMessages2() {
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
 	}
 
 }
